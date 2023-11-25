aa741cb03fed3122eb0f638e861d1f23a0c384e6
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-11-25 21:16:22 +0330

    calculate Elapsed time

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from engine import CNF
+import time
+
+# Start timer
+start_time = time.time()
 
 text = ["and", "A", ["and", "B", "C"]]
 cnf1 = CNF(text)
 print(cnf1.reduceOperators(text))
+
+# End timer
+end_time = time.time()
+# Calculate elapsed time
+elapsed_time = end_time - start_time
+print("Elapsed time: {}s".format(elapsed_time))
