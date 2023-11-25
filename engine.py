68747b32b3a0621dcf1f33681405be965da5e57f
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-11-25 21:05:37 +0330

    create reduceOperators

--- engine.py
+++ engine.py
@@ -8,3 +8,28 @@ class CNF:
 
     def __contains__(self):
         print("Contains is called!")
+
+    # Reduces the operators, if "and" is present inside "and" or "or" inside "or"
+    # Example : ["and", "A", ["and", "B", "C"]] should be ["and", "A", "B", "C"]
+    def reduceOperators(self):
+        if (isinstance(self.propositional, str)):
+            return self.propositional
+        operator = self.propositional[0]
+        literals = []
+        propositions = []
+        for index, item in enumerate(self.propositional):
+            if (index > 0):
+                if (isinstance(item, str)):
+                    literals.append(item)
+                elif (isinstance(item, list)):
+                    propositions.append(self.reduceOperators(item))
+        newFormula = literals
+        for item in propositions:
+            if (isinstance(item, list) and item[0] == operator):
+                for i, clause in enumerate(item):
+                    if (i > 0):
+                        newFormula.append(clause)
+            else:
+                newFormula.append(item)
+        newFormula.insert(0, operator)
+        return newFormula
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from engine import CNF
 
-text = "test massage"
+text = [["and", "A", ["and", "B", "C"]]]
 cnf1 = CNF(text)
+print(cnf1.reduceOperators())
