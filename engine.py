828c4397e1a7f7da579078d03e3ee0b60867057b
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-11-26 18:04:13 +0330

    convert

--- engine.py
+++ engine.py
@@ -98,3 +98,116 @@ class CNF:
         newFormula = literals + propositions
         newFormula.insert(0, operator)
         return newFormula
+
+    # Converts to CNF by taking different cases separately
+    def convert(self, formula):
+        if (isinstance(formula, str)):
+            return formula
+        elif (isinstance(formula, list)):
+            # A => B    --->    ~A | B
+            if (formula[0] == "implies"):
+                return convert(["or", convert(["not", convert(formula[1])]), convert(formula[2])])
+            # A <=> B   --->    (~A | B) & (A | ~B)
+            elif (formula[0] == "iff"):
+                return convert("and", convert(["or", convert(["not", formula[1]]), formula[2]]),
+                               convert(["or", formula[1], convert(["not", formula[2]])]))
+            elif (formula[0] == "not"):
+                # ~p
+                if (isinstance(formula[1], str)):
+                    return formula
+                # ~~p   --->    p
+                elif (isinstance(formula[1], list) and (formula[1])[0] == "not"):
+                    return convert((formula[1])[1])
+                # ~(A & B & C & ...)  --->    ~A | ~B | ~C | ....
+                elif (isinstance(formula[1], list) and (formula[1])[0] == "and"):
+                    disjuncts = []
+                    for index, item in enumerate(formula[1]):
+                        if (index > 0):
+                            disjuncts.append(convert(["not", item]))
+                    disjuncts.insert(0, "or")
+                    return convert(disjuncts)
+                # ~(A | B | C | ...)  --->    ~A & ~B & ~C & ....
+                elif (isinstance(formula[1], list) and (formula[1])[0] == "or"):
+                    conjuncts = []
+                    for index, item in enumerate(formula[1]):
+                        if (index > 0):
+                            conjuncts.append(convert(["not", item]))
+                    conjuncts.insert(0, "and")
+                    return convert(conjuncts)
+                # ~(A => B)	--->	A & ~B
+                elif (isinstance(formula[1], list) and ((formula[1])[0] == "implies")):
+                    return convert(["and", convert((formula[1])[1]), ["not", convert((formula[1][2]))]])
+                elif (isinstance(formula[1], list) and (formula[1])[0] == "iff"):
+                    return convert(["not", convert(formula[1])])
+            elif (formula[0] == "or"):
+                # A | A  --->    A
+                formula = sort(formula)
+                formula = removeDuplicates(formula)
+                # Handling the case ["or", "A", ["or", "B", "C"]]
+                formula = reduceOperators(formula)
+                # The order will be messed up when the redundant operators are removed
+                # Handling the case when the formula is reduced.
+                # For instance A or A is reduced to A
+                formula = sort(formula)
+                if (len(formula) == 1):
+                    return formula
+                if ((isinstance(formula[-1], list) and (formula[-1])[0] == "and")):
+                    # A | (B & C & D & ...)  --->  (A | B) & (A | C) & (A | D) & ...
+                    conjuncts = []
+                    for i, item in enumerate(formula[-1]):
+                        if (i > 0):
+                            conjuncts.append(["or", formula[-2], item])
+                    conjuncts.insert(0, "and")
+                    # If only 2 items, then remove them and also remove OR
+                    if (len(formula) < 4):
+                        return convert(conjuncts)
+                    else:
+                        formula.remove(formula[-1])
+                        formula.remove(formula[-1])
+                    formula.append(conjuncts)
+                    return convert(formula)
+                # Case A OR B,
+                elif ((isinstance(formula[1], str) and isinstance(formula[2], str)) or (
+                        isinstance(formula[1], str) and isinstance(formula[2], list) and (formula[2])[
+                    0] == "not" and isinstance((formula[2])[1], str)) or (
+                              isinstance(formula[2], str) and isinstance(formula[1], list) and (formula[1])[
+                          0] == "not" and isinstance((formula[1])[1], str))):
+                    formula.append(["or", formula[1], formula[2]])
+                    formula.remove(formula[1])
+                    formula.remove(formula[1])
+
+                    formula = reduceOperators(formula)
+                    formula = sort(formula)
+                    return formula
+                # Case !A OR !B
+                elif (isinstance(formula[1], list) and (formula[1])[0] == "not" and isinstance((formula[1])[1],
+                                                                                               str) and isinstance(
+                        formula[2], list) and (formula[2])[0] == "not" and isinstance((formula[2])[1], str)):
+                    return formula
+                # For any other operator compute the inner operator after or
+                else:
+                    disjuncts = []
+                    for i, item in enumerate(formula):
+                        if (i > 0):
+                            disjuncts.append(convert(item))
+                    disjuncts.insert(0, "or")
+                    return convert(disjuncts)
+
+            elif (formula[0] == "and"):
+                # Handling the case ["and", "A", ["or", "C", "D"], ["or", "D", "C"]]
+                formula = sort(formula)
+                formula = removeDuplicates(formula)
+                # Handling the case ["and", "A", ["and", "B", "C"]]
+                formula = reduceOperators(formula)
+                # The order will be messed up when the redundant operators are removed
+                formula = sort(formula)
+                if (len(formula) == 1):
+                    return formula
+                disjuncts = []
+                for i, item in enumerate(formula):
+                    if (i > 0):
+                        disjuncts.append(convert(item))
+                disjuncts.insert(0, "and")
+
+                disjuncts = reduceOperators(disjuncts)
+                return disjuncts
